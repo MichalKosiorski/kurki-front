a535afbbd44c1e7968ab65aeb59ec52af31fc463
MichalKosiorski <118568257+MichalKosiorski@users.noreply.github.com>
2026-06-02 15:00:22 +0200

    test commit 6
    checking pipeline

--- src/pages/registration-login/loginPage.jsx
+++ src/pages/registration-login/loginPage.jsx
@@ -1,9 +1,16 @@
+import { useRef } from "react";
 import { HeaderH1, SimpleButton, Spacer } from "../../scaffolding/simple-elements";
+import { SimpleInput } from "../../scaffolding/simple-form-elements";
 import "./registration.css"
+import { RiMailLine } from "react-icons/ri";
+
 
 export default function LoginPage(
 
 ){
+
+    const loginRef = useRef();
+
     return <main>
         <Spacer height_pc={100}/>
         <div className="grid-1-1">
@@ -11,6 +18,10 @@ export default function LoginPage(
                 <Spacer height_pc={20}/>
                 <HeaderH1 text={"Zaloguj się!"} pc_align="center" phone_aling="center" tablet_align="center" color={"var(--dark-green)"}/>
                 <Spacer height_pc={20}/>
+            </div>
+            <div className="registration-right">
+                <Spacer height_pc={20}/>
+                <SimpleInput ref={loginRef} svg={<RiMailLine/>} placeholder="Podaj email"/>
             </div>
         </div>
         <Spacer height_pc={160}/>
--- src/scaffolding/simple-form-elements.jsx
+++ src/scaffolding/simple-form-elements.jsx
@@ -1,30 +1,49 @@
 import "./simple-elements.css";
 
 
-export function SimpleInput({
-    type = 'text',
-    onEnter = null,
-    onChange = null,
-    value = null,
-    placeholder = "",
-    add_class = "",
-    id_param = ''
-})
-{
-    function handleOnChange(v){
-        if(onChange)
-            onChange(v);
+import { forwardRef } from "react";
+import "./simple-elements.css";
+
+export const SimpleInput = forwardRef(function SimpleInput(
+    {
+        type = 'text',
+        onEnter = null,
+        onChange = null,
+        value = null,
+        svg = null,
+        placeholder = "",
+        add_class = "",
+        id_param = ''
+    },
+    ref
+) {
+    function handleOnChange(v) {
+        if (onChange) onChange(v);
+    }
+
+    function handleKeyDown(e) {
+        if (e.key === "Enter" && onEnter) {
+            onEnter(e.target.value);
+        }
     }
 
     return (
         <div className={`simple-input ${add_class}`}>
+            {svg && <div className="simple-input-left">
+                {svg}
+            </div>}
             <input 
+                ref={ref}
                 id={id_param}
                 type={type}
                 placeholder={placeholder}
                 value={value}
-                onChange={(e)=>{handleOnChange(e.target.value)}}
+                onChange={(e) => { handleOnChange(e.target.value) }}
+                onKeyDown={handleKeyDown}
             />
+            {type == 'password' && <div className="simple-input-right">
+
+            </div>}
         </div>
-    )
-}
+    );
+});
